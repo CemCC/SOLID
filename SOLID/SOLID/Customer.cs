ca52320ce0e91c78caf71e88b64823ec47c161ed
CemCC <cemcaliskan@ymail.com>
2018-06-14 22:40:31 +1000

    Problem identification

--- SOLID/SOLID/Customer.cs
+++ SOLID/SOLID/Customer.cs
@@ -6,15 +6,16 @@ using System.Threading.Tasks;
 
 namespace SOLID
 {
+    // Aim is to add the customers 
     class Customer
-    {
+    {   // Principle: Every class should have single responsibility
         public void Add()
         {
             try
             {
                 Console.WriteLine("Added Customer");
             }
-            catch (Exception ex)
+            catch (Exception ex) // Problem : This class is created only for adding customers.
             {
                 Console.WriteLine($"Cannot add customer due to {ex}");
             }
