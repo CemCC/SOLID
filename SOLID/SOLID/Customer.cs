f657bb58267b52dcae99db25de8aaedacadfefba
CemCC <cemcaliskan@ymail.com>
2018-06-14 22:38:37 +1000

    single responsibility

---
+++ SOLID/SOLID/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID
+{
+    class Customer
+    {
+        public void Add()
+        {
+            try
+            {
+                Console.WriteLine("Added Customer");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Cannot add customer due to {ex}");
+            }
+        }
+    }
+}
---
+++ SOLID/SOLID/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var Customer = new Customer();
+            Customer.Add();
+
+            Console.ReadLine();
+        }
+    }
+}
