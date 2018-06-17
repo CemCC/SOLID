45046a0b986bf6191204d5b27f5e459da9ae728d
CemCC <cemcaliskan@ymail.com>
2018-06-17 12:53:12 +1000

    Calculate Mercedes Cost

---
+++ SOLID/SOLID/OpenClose/EstimatedCost.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.OpenClose
+{
+    public class EstimatedCost
+    {   /// <summary>
+    /// We are writing program for car dealership.
+    /// It needs to calculate estimated cost.
+    /// Here we have written a program which returns Mercedes Costs
+    /// </summary>
+    /// <param name="cars"></param>
+    /// <returns></returns>
+        public double Cost(Mercedes[] cars)
+        {
+            double cost = 0;
+            foreach (var car in cars)
+            {
+                cost += car.Cost;
+            }
+            return cost;
+        }
+    }
+}
