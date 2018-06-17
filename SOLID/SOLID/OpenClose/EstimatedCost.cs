d74c7761f4c8442a395afa23f589f99a33429517
CemCC <cemcaliskan@ymail.com>
2018-06-17 13:08:47 +1000

    Add Toyota cost calculation

--- SOLID/SOLID/OpenClose/EstimatedCost.cs
+++ SOLID/SOLID/OpenClose/EstimatedCost.cs
@@ -11,15 +11,32 @@ namespace SOLID.OpenClose
     /// We are writing program for car dealership.
     /// It needs to calculate estimated cost.
     /// Here we have written a program which returns Mercedes Costs
+    /// Now we have another problem. A dealership can have more than Mercedes Models.
+    /// For example Dealership receives Toyota brand car in to the shop... 
     /// </summary>
     /// <param name="cars"></param>
     /// <returns></returns>
-        public double Cost(Mercedes[] cars)
+        public double Cost(object[] cars)
         {
             double cost = 0;
             foreach (var car in cars)
             {
-                cost += car.Cost;
+                if (car is Mercedes)
+                {
+                    Mercedes mercedes = (Mercedes)car;
+                    cost += mercedes.Cost;
+                }
+                else if (car is Toyota) {
+                    /// we have added Toyota as in if condition. And It will calculate Toyota Cost. 
+                    /// However, What if dealership receives Hyundai, BMW, Holden or anyother Type?
+                    /// This method will get bigger and bigger. We need a new if condition for each Car Model.
+                    /// *Desirable method should be close to the Modification, even though new car models are added.
+                    /// *SO, Classes should be extensible.
+                    /// This Code still has got work to meet Open-Close principle.
+                    /// Let's fix that.!
+                    Toyota toyota = (Toyota)car;
+                    cost += toyota.Cost;
+                }
             }
             return cost;
         }
